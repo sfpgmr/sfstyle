ba52852a2689769e8b93f5fbf9e961a38d5e234e
sfgpmr <sfpgmr@dummy.com>
2015-09-16 07:52:13 +0900

    update

--- src/js/sfstyle.js
+++ src/js/sfstyle.js
@@ -1,4 +1,14 @@
 (() => {
-	var aa = 'bb';
-//	alert('test');
+  d3.selectAll('.nav-button')
+  .on('click',function(d,i){
+      var target = d3.select('.main-nav[data-sf-name="' + d3.select(this).attr('data-sf-target') + '"] .nav-items');
+      console.log(target);
+      if(target){
+        if(target.style('display') === 'none'){
+          target.style({'display':'flex','flex-direction':'column'});
+        } else {
+          target.style({'display':'none','flex-direction':'row'});
+        }
+      }
+  });
 })();
