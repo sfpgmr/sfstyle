44bdf1bdaae0d45021698e42b508b8308bd25c95
sfgpmr <sfpgmr@dummy.com>
2015-09-18 06:18:42 +0900

    update

--- src/js/sfstyle.js
+++ src/js/sfstyle.js
@@ -2,13 +2,20 @@
   d3.selectAll('.nav-button')
   .on('click',function(d,i){
       var target = d3.select('.main-nav[data-sf-name="' + d3.select(this).attr('data-sf-target') + '"] .nav-items');
-      console.log(target);
       if(target){
         if(target.style('display') === 'none'){
-          target.style({'display':'flex','flex-direction':'column'});
+          target.style({'display':'flex'});
         } else {
-          target.style({'display':'none','flex-direction':'row'});
+          target.style({'display':'none'});
         }
       }
+   }).each(function(){
+    var self = this;
+    d3.select(window).on('resize',function(){
+      var target = d3.select('.main-nav[data-sf-name="' + d3.select(self).attr('data-sf-target') + '"] .nav-items');
+      if(target.style('flex-direction') === 'row'){
+          target.style({'display':'flex'});
+      }
+    });
   });
 })();
